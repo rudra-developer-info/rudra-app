e3711043e171bd61af0e2561305c7ca4eb15b719
rudra-developer-info <rudradeveloper.info@gmail.com>
2024-08-31 23:21:19 +0530

    Update settings.js
    backendPort changed

--- api/settings.js
+++ api/settings.js
@@ -1,5 +1,5 @@
 export const frontEndPort = 'https://rudra-app-frontend.onrender.com';
-export const backEndPort = 'https://rudra-app-backend.onrender.com';
+export const backEndPort = 'https://rudra-app-backend-7geo.onrender.com';
 
 // local config.js
 // export const frontEndPort = 'http://localhost:5173';
